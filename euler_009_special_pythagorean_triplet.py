de6016bb05a9cc57e657bb9d7f27cf5c593a2ee1
Josh Johansen <josh@Now.local>
2018-03-11 23:19:23 -0700

    Added euler 9 outline.

---
+++ euler_009_special_pythagorean_triplet.py
@@ -0,0 +1,23 @@
+'''
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+'''
+
+product = 0
+
+def bruteforce():
+    return product
+    
+def optimized():
+    return product
+
+optimized_solution = optimized()
+bruteforce_solution = bruteforce()
+
+print ('Bruteforce:{}'.format(bruteforce_solution))
+print ('Optimized:{}'.format(optimized_solution))
